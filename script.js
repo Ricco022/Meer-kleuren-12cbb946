105945b364011aebd48bee7fbb30317f60e7a456
Riccardo van der Horst <riccardo271002@gmail.com>
2020-01-28 11:36:25 +0100

    Uitwerking

---
+++ script.js
@@ -0,0 +1,15 @@
+var backgroundColor = null;
+var titleColor = null;
+function bg() {
+    backgroundColor = document.getElementById("backg").value;
+}
+
+function titlecolor() {
+    titleColor = document.getElementById("backg").value;
+}
+
+function applycolor() {
+    document.getElementById("body").style.backgroundColor = backgroundColor;
+    document.getElementById("title").style.color = titleColor;
+}
+
